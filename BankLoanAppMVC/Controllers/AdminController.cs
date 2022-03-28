f68eecda15a0230f0ca4864b27827f1faf48601c
ManikandanKannanK <100766221+ManikandanKannanK@users.noreply.github.com>
2022-03-28 15:11:16 +0530

    Add project files.

---
+++ BankLoanAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,176 @@
+﻿using BankLoanAppMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankLoanAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        private AppDBContext db = new AppDBContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Admin admins)
+        {
+
+
+            var obj = db.Admins
+                    .Where(a => a.AdminName.Trim() == admins.AdminName &&
+                                a.Password.Trim() == admins.Password).FirstOrDefault();
+
+            if (obj != null)
+            {
+                Session["AdminName"] = obj.AdminName.ToString();
+                ViewBag.Message = "Login Successfully.!";
+
+                return View();
+            }
+            else
+            {
+                ViewBag.Message = "User not found for given Email and Password";
+                return View();
+            }  
+
+    }
+
+        public ActionResult ChangeStatus()
+        {
+            if (Session["AdminName"] == null)
+            {
+                
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(db.Loans.ToList());
+
+            }
+            
+            
+
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Loan loans = db.Loans.Find(id);
+            if (loans == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loans);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Loan loan)
+        {
+            if (ModelState.IsValid)
+            {
+                loan.Status = "Approved";
+                db.Entry(loan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ChangeStatus");
+            }
+            return View(loan);
+        }
+
+        public ActionResult Decline(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Loan loans = db.Loans.Find(id);
+            if (loans == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loans);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Decline(Loan loan)
+        {
+            if (ModelState.IsValid)
+            {
+                loan.Status = "Declined";
+                db.Entry(loan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ChangeStatus");
+            }
+            return View(loan);
+        }
+
+        public ActionResult ViewCustomers()
+        {
+            if (Session["AdminName"] == null)
+            {
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(db.Customers.ToList());
+
+            }
+
+            
+            
+            
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customers = db.Customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+           
+            Customer customers = db.Customers.Find(id);
+            db.Customers.Remove(customers);
+            db.SaveChanges();
+
+            Loan loans = db.Loans.Where(s => s.CustomerID == id).FirstOrDefault();
+            db.Loans.Remove(loans);
+            db.SaveChanges();
+
+            return RedirectToAction("ViewCustomers");
+        }
+
+
+
+    }
+}
---
+++ BankLoanAppMVC/Controllers/CustomerController.cs
@@ -0,0 +1,129 @@
+﻿using BankLoanAppMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankLoanAppMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public int idval;
+        private AppDBContext db = new AppDBContext();
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Customer model)
+        {
+
+
+            var obj = db.Customers.Where(a => a.Email.Trim() == model.Email &&
+                                                a.Password.Trim() == model.Password).FirstOrDefault();
+
+            if (obj != null)
+            {
+                Session["UserID"] = obj.CustomerID.ToString();
+                Session["Email"] = obj.Email.ToString();
+                idval = Convert.ToInt16(Session["UserID"]);
+                ViewBag.Message = "Login Successfully.!";
+                return View();
+            }
+            else
+            {
+                ViewBag.Message = "User not found for given Email and Password";
+                return View();
+            }
+
+
+        }
+
+        public ActionResult Register()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Customer user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(user);
+                db.SaveChanges();
+                ViewBag.Message = "Registered Successfully";
+                return View();
+            }
+
+            return View();
+
+        }
+
+
+        public ActionResult LoanApply()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult LoanApply(Loan loan)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                loan.CustomerID = Convert.ToInt32(Session["UserID"]);
+                loan.Status = "OnProgress..!!";
+                db.Loans.Add(loan);
+                db.SaveChanges();
+                ViewBag.Message = "Applied Successfully";
+                return View();
+            }
+
+            return View();
+
+        }
+
+
+
+
+
+
+
+        public ActionResult LoanStatus()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View(db.Loans.ToList());
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+
+        }
+
+
+    }
+}
